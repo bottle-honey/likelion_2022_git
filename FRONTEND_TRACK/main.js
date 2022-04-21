713e653461727378e5a3694a1265e9ad395b8d58
bottle-honey <honey_sheep@naver.com>
2022-04-21 23:43:43 +0900

    #1

---
+++ FRONTEND_TRACK/main.js
@@ -0,0 +1,51 @@
+// // 변수선언 및 데이터할당
+// var num = 3;
+// console.log(num);
+
+// let str = "a";
+// console.log(str);
+
+// const constant = 10;
+// console.log(constant);
+
+// //원시 타입
+// let number = 1;
+// let stt = "abc";
+// let bool = true;
+// let undi = undefined;
+// let nul = null;
+// let symbol = Symbol();
+
+// //참조 타입
+// let array = [];
+// let obj = {};
+// let func = function(){};
+
+
+// console.log(typeof(nul));
+
+// let integer = 10;
+// let hex = 0xa;
+
+// //special
+// let IsInfinity = 10/0;
+// let IsNaN = 10/"일";
+
+// //error (부동소수점 오류)
+// let sum = 0.1+0.2;
+
+// //string
+// let str = "abc";
+// let str2 = 'abc';
+
+// let str3 = "I'm fine thank you!\" and you?\"";
+
+// console.log(str3);
+
+// undefined, null
+
+// let srt;
+// console.log(str);
+
+// let num = null;
+// console.log(num);
